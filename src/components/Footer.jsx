c6cf4f20cfd044512f75c4c7b201e0f8a369f92e
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-03-26 20:04:31 +0100

    Updated the footer and added a whole new looks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,46 +1,102 @@
+import { Link } from "react-router-dom";
+
 export default function Footer({ site_name, siteDesc }) {
+  const links1 = [
+    {
+      name: "404 page",
+      url: "/404-page",
+    },
+    {
+      name: "Home",
+      url: "/",
+    },
+    {
+      name: "Sports",
+      url: "/sports",
+    },
+    {
+      name: "Posts",
+      url: "/posts",
+    },
+    {
+      name: "News",
+      url: "/news",
+    },
+    {
+      name: "About",
+      url: "/about",
+    },
+  ];
   return (
     <>
       <div className="mt-5 p-5 md:py-8 px-5 md:px-10 bg-cs-dark-light">
         <div className="block md:flex justify-between">
-          <div className="w-1/3">
+          <div className="w-full md:w-1/3">
             <div className="text-white font-bold text-4xl">{site_name}</div>
-            <div className="text-gray-400 max-w-[60%] mt-1">
-              {siteDesc}
+            <div className="text-gray-400 max-w-[60%] mt-1">{siteDesc}</div>
+          </div>
+          <div className="mt-5 md:mt-0 w-full w-3/4 block md:flex gap-x-2 justify-between">
+            {/* footer link container */}
+            <div className="mt-1 md:mt-0 w-full md:w-1/3 p-2">
+              <div className="font-bold text-xl text-white">Links (Pages)</div>
+              <div className="mt-2">
+                {links1.map((link, index) => (
+                  <div key={index}>
+                    <a
+                      href={link.url}
+                      className="text-gray-400 duration-300 hover:text-white"
+                    >
+                      {link.name}
+                    </a>
+                  </div>
+                ))}
+              </div>
+            </div>
+            {/* footer link container */}
+            <div className="mt-1 md:mt-0 w-full md:w-1/3 p-2">
+              <div className="font-bold text-xl text-white">Legal</div>
+              <div>
+                  <a
+                    href="/t&c"
+                    className="text-gray-400 duration-300 hover:text-white"
+                  >
+                    Terms & Conditions
+                  </a>
+                </div>
+                <div>
+                  <a
+                    href="/privacy-policy"
+                    className="text-gray-400 duration-300 hover:text-white"
+                  >
+                    Privacy Policy
+                  </a>
+                </div>
+            </div>
+            {/* footer link container */}
+            <div className="mt-1 md:mt-0 w-full md:w-1/3 p-2">
+              <div className="font-bold text-xl text-white">Extra (Other)</div>
+              <div className="mt-2">
+                <div>
+                  <a
+                    href="#"
+                    className="text-gray-400 duration-300 hover:text-white"
+                  >
+                    Credits
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="mt-5 md:mt-0 w-3/4 block md:flex gap-x-2 justify-between">
-            {/* footer link container */}
-            <div className="mt-2 md:mt-0 w-full md:w-1/3 p-2">
-              <div className="font-bold text-xl text-white">Links</div>
-              <div className="mt-2">
-                <div><a href="#" className="text-gray-400 duration-300 hover:text-white">Link</a></div>
-                <div><a href="#" className="text-gray-400 duration-300 hover:text-white">Link</a></div>
-                <div><a href="#" className="text-gray-400 duration-300 hover:text-white">Link</a></div>
-                <div><a href="#" className="text-gray-400 duration-300 hover:text-white">Link</a></div>
-                <div><a href="#" className="text-gray-400 duration-300 hover:text-white">Link</a></div>
-              </div>
-            </div>
-            {/* footer link container */}
-            <div className="mt-2 md:mt-0 w-full md:w-1/3 p-2">
-              <div className="font-bold text-xl text-white">Links</div>
-              <div className="mt-2">
-                <div><a href="#" className="text-gray-400 duration-300 hover:text-white">Link</a></div>
-                <div><a href="#" className="text-gray-400 duration-300 hover:text-white">Link</a></div>
-                <div><a href="#" className="text-gray-400 duration-300 hover:text-white">Link</a></div>
-                <div><a href="#" className="text-gray-400 duration-300 hover:text-white">Link</a></div>
-              </div>
-            </div>
-            {/* footer link container */}
-            <div className="mt-2 md:mt-0 w-full md:w-1/3 p-2">
-              <div className="font-bold text-xl text-white">Links</div>
-              <div className="mt-2">
-                <div><a href="#" className="text-gray-400 duration-300 hover:text-white">Link</a></div>
-                <div><a href="#" className="text-gray-400 duration-300 hover:text-white">Link</a></div>
-                <div><a href="#" className="text-gray-400 duration-300 hover:text-white">Link</a></div>
-              </div>
-            </div>
-          </div>
+        </div>
+        {/* end flex */}
+        <div className="w-[100%] my-3">
+          <div className="w-full bg-gray-700 h-[1px]"></div>
+        </div>
+        <div className="text-center text-white font-semibold text-xl">
+          &copy; {site_name} 2024 - Built by{" "}
+          <a href="https://okekedivine.vercel.app" target={"_blank"}>
+            <span className="cs-text-yellow-1">Dev Divine</span>
+          </a>
         </div>
       </div>
     </>
