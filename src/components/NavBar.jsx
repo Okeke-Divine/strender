f17692c02154f8e3f3187238faf2d0dd84cdc7b4
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-03-26 16:56:22 +0100

    Made the navbar a component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,49 @@
+export default function NavBar({site_name}) {
+  const links = [
+    { url: "#", name: "Home" },
+    { url: "#", name: "Sports" },
+    { url: "#", name: "Posts" },
+    { url: "#", name: "About" },
+    { url: "#", name: "Contact" },
+  ];
+  return (
+    <>
+      <div className="p-5 md:py-8 px-5 md:px-10 bg-cs-dark-light">
+        <div className="flex justify-between items-center">
+          <div className="block sm:flex items-center sm:gap-x-5 md:gap-x-16 lg:gap-x-32 duration-300">
+            <div className="text-white font-bold text-2xl">{site_name}</div>
+            <div className="hidden sm:flex items-center sm:gap-x-3 md:gap-x-5 duration-300">
+              {links.map((link, index) => (
+                <div>
+                  <a
+                    className={`${
+                      link.name == "Home"
+                        ? "text-white font-bold border-b-[2px]"
+                        : "text-gray-400"
+                    } border-yellow-300 duration-300 hover:text-white`}
+                    href={link.url}
+                  >
+                    {link.name}
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
+          {/* login and signup button */}
+          <div className="flex gap-x-4">
+            <div>
+              <button className="duration-300 hover:bg-gray-600 hover:text-white rounded-3xl border-[1px] text-gray-200 border-gray-200 p-2 px-5 cursor-pointer">
+                Login
+              </button>
+            </div>
+            <div>
+              <button className="duration-300 hover:bg-gray-700 rounded-3xl border-[1px] text-white bg-gray-600 border-gray-200 p-2 px-5 cursor-pointer">
+                Signup
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
