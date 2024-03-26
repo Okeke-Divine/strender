3303a27c6d8aae5f582a53f4c06d8f6a286ba1e6
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-03-26 16:05:26 +0100

    updated the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+// data files
+import applicationConfig from "./data/applicationConfig.json"
+
 function App() {
+  useEffect(function(){
+    document.title = applicationConfig["site_name"];
+  },[])
   return (
     <>
       <div>blog</div>
