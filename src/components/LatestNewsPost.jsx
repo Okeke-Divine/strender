3ab78df0fb66c9e13fa1bf2bb570028a662bb9d4
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-03-27 09:59:14 +0100

    Updated the site

--- src/components/LatestNewsPost.jsx
+++ src/components/LatestNewsPost.jsx
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom";
 export default function LatestNewsPost({ title, previewText, url, thumbnail }) {
   return (
     <div className="hover:opacity-90 duration-300">
-      <div>
+      <div className="flex flex-col justify-between h-full">
         <div className="w-full bg-blue-200 h-[200px] rounded-lg p-2"></div>
         <div className="mt-3 font-bold text-xl text-white">{title}</div>
         <div className="text-gray-400 mt-2">{previewText}</div>
-        <div>
-          <Link to={url} className="cursor-pointer">
-            <button className="text-white">Read more</button>
+        <div className="mt-2">
+          <Link
+            to={url}
+            className="cursor-pointer cs-text-yellow-1 duration-300 font-bold"
+          >
+            Read More
           </Link>
         </div>
       </div>
