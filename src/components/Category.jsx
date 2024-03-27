d12ae28610f3fa24093854e92acbf38c0a1e2b2e
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-03-27 09:52:17 +0100

    Updated

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,14 +1,24 @@
 //components
 import ComponentTitle from "./__global/ComponentTitle";
-import CategoryCardCircle from "./CategoryCardCircle"
+import CategoryCardCircle from "./CategoryCardCircle";
 
 export default function Category() {
+  const categories = [{ title: "Football", url: "/sports/football" }];
   return (
     <>
       <div className="mt-5">
         <ComponentTitle title="Category" url="/category" />
         {/* list of sports */}
         <div className="flex gap-x-5 overflow-auto whitespace-nowrap mt-2">
+          {categories.map((category, index) => (
+            <>
+              <CategoryCardCircle
+                title={category.title}
+                url={category.url}
+                key={index}
+              />
+            </>
+          ))}
           <CategoryCardCircle title="Footbal" url="/sports/football" />
           <CategoryCardCircle title="Basketball" url="/sports/football" />
           <CategoryCardCircle title="Boxing" url="/sports/football" />
