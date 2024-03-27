1670f2fc89d6995f6f826073c4227c3564359e49
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-03-27 08:14:56 +0100

    Added the landinPage component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,7 +32,9 @@ export default function Footer({ site_name, siteDesc }) {
       <div className="mt-5 p-5 md:py-8 px-5 md:px-10 bg-cs-dark-light">
         <div className="block md:flex justify-between">
           <div className="w-full md:w-1/3">
-            <div className="text-white font-bold text-4xl">{site_name}</div>
+            <div className="text-white font-bold text-4xl cursor-pointer">
+              <Link to="/">{site_name}</Link>
+            </div>
             <div className="text-gray-400 max-w-[60%] mt-1">{siteDesc}</div>
           </div>
           <div className="mt-5 md:mt-0 w-full md:w-3/4 block md:flex gap-x-2 justify-between">
@@ -56,21 +58,21 @@ export default function Footer({ site_name, siteDesc }) {
             <div className="mt-1 md:mt-0 w-full md:w-1/3 p-2">
               <div className="font-bold text-xl text-white">Legal</div>
               <div>
-                  <a
-                    href="/t&c"
-                    className="text-gray-400 duration-300 hover:text-white"
-                  >
-                    Terms & Conditions
-                  </a>
-                </div>
-                <div>
-                  <a
-                    href="/privacy-policy"
-                    className="text-gray-400 duration-300 hover:text-white"
-                  >
-                    Privacy Policy
-                  </a>
-                </div>
+                <a
+                  href="/t&c"
+                  className="text-gray-400 duration-300 hover:text-white"
+                >
+                  Terms & Conditions
+                </a>
+              </div>
+              <div>
+                <a
+                  href="/privacy-policy"
+                  className="text-gray-400 duration-300 hover:text-white"
+                >
+                  Privacy Policy
+                </a>
+              </div>
             </div>
             {/* footer link container */}
             <div className="mt-1 md:mt-0 w-full md:w-1/3 p-2">
