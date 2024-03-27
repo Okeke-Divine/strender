8807da88ac98038a59136dd0ded5b36bcb1b4178
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-03-27 09:39:57 +0100

    added the new posts element and made major update to the site

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,16 +1,38 @@
 //components
 import ComponentTitle from "../components/__global/ComponentTitle";
+import LatestNewsPost from "../components/LatestNewsPost";
 
-export default function LatestNews({lastestNewsDesc}) {
+export default function LatestNews({ lastestNewsDesc }) {
+  const lastestNews = [
+    {
+      title: "Real madrid to be suspended from Master league",
+      previewText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil eius beatae iusto enim dignissimos, itaque impedit aliquid minima praesentium recusandae delectus inventore nobis harum voluptates expedita sint? Voluptatibus, accusamus harum?",
+      url: "",
+      thumbnail: "",
+    }
+  ]
   return (
     <>
-      <div className="mt-5">
-        <ComponentTitle title="Latest News" url="/news?sort_by=latest" />
-        <div className="mt-1 text-gray-400 font-semibold">
-          {lastestNewsDesc}
+        <div className="mt-5">
+          <ComponentTitle title="Latest News" url="/news?sort_by=latest" />
+          <div className="mt-1 text-gray-400 font-semibold">
+            {lastestNewsDesc}
+          </div>
+          <div className="mt-5">
+            <div className="grid grid-cols-1 md:md:grid-cols-2 lg:grid-cols-3 gap-5">
+              <LatestNewsPost />
+              <LatestNewsPost />
+              <LatestNewsPost />
+              <LatestNewsPost />
+              <LatestNewsPost />
+              <LatestNewsPost />
+              <LatestNewsPost />
+            </div>
+            <div className="mt-1">
+              <button>Read more</button>
+            </div>
+          </div>
         </div>
-        <div></div>
-      </div>
     </>
   );
 }
--- src/components/MostReadPostElem.jsx
+++ src/components/MostReadPostElem.jsx
@@ -4,11 +4,11 @@ export default function MostReadPostElem({ url, title, author, date, views }) {
   return (
     <>
       <Link to={url} className="w-full">
-        <div className="flex gap-x-3 cursor-pointer duration-300 hover:opacity-70">
-          <div className="w-1/2 md:w-1/3">
+        <div className="block md:flex gap-x-3 cursor-pointer duration-300 hover:opacity-70">
+          <div className="w-full md:w-1/3">
             <div className="bg-blue-200 p-5 w-full h-[90px] rounded-lg"></div>
           </div>
-          <div className="w-1/2 md:w-2/3">
+          <div className="mt-2 md:mt-0 w-full md:w-2/3">
             <div className="font-bold text-white">{title}</div>
             <div className="text-white">{author}</div>
             <div className="block md:flex text-gray-400 gap-x-2 justify-between items-center">
