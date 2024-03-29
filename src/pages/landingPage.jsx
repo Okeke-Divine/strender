c86ece2dfe604940b9bb4c853e11addff7bf27c6
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-03-29 20:46:41 +0100

    Fixed a terrible bug

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,7 +1,31 @@
-export default function LandingPage({site_name}) {
+import { Link } from "react-router-dom";
+
+export default function LandingPage({ site_name }) {
   return (
     <>
-      <div>welcome to {site_name}</div>
+      <main className="mainComponent">
+        <div className="text-white text-4xl font-bold">Welcome.</div>
+        <div className="mt-5 text-xl text-white">
+          This page is still on under construction. <br />
+          In the meantime you can navigate to{" "}
+          <span className="text-yellow-400">
+            {" "}
+            <Link to="/home">Home</Link>
+          </span>
+        </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </main>
     </>
   );
 }
