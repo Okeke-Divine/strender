e14abd3021f92d57f457481e603e509e6ff3ef9d
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-03 11:44:58 +0100

    Updated the privacy policy

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -1,7 +1,20 @@
-export default function PrivacyPolicy(){
-    return (
-        <>
-            <div>PrivacyPolicy</div>
-        </>
-    )
+import PrivacyPolicyData from "../data/PrivacyPolicy.json";
+
+export default function PrivacyPolicy() {
+  return (
+    <>
+      <main className="mainComponent">
+        {PrivacyPolicyData.sections.map((section, index) => (
+          <div key={index} className="mb-8">
+            <h2 className="text-3xl font-bold text-white mb-4">
+              {section.title}
+            </h2>
+            {section.content.map((content, idx) => (
+              <p key={idx} className="text-gray-400">{content}</p>
+            ))}
+          </div>
+        ))}
+      </main>
+    </>
+  );
 }
