622d2adff73475cabb729223b1847bae495aea42
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-03 12:59:00 +0100

    Added the dynmaic titles

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,9 +1,10 @@
 import DocumentTittle from "../utils/documentTitle";
 
-export default function Category(){
-    return (
-        <>
-            <div className="text-xl text-white">Http server esponded - Category</div>
-        </>
-    )
+export default function Category() {
+  return (
+    <>
+      <DocumentTittle title="Category" />
+      <div className="text-xl text-white">Http server esponded - Category</div>
+    </>
+  );
 }
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,10 @@
 import DocumentTittle from "../utils/documentTitle";
 
-export default function Discover(){
-    return (
-        <>
-            <div className="text-xl text-white">Http server esponded - Discover</div>
-        </>
-    )
+export default function Discover() {
+  return (
+    <>
+      <DocumentTittle title="Discover" />
+      <div className="text-xl text-white">Http server esponded - Discover</div>
+    </>
+  );
 }
