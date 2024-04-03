2a1de2db75563896bac57fc3ac6eaa723d67074f
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-03 13:12:28 +0100

    Updated the landing page -> Redirct to home

--- src/pages/DefaultPage.jsx
+++ src/pages/DefaultPage.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 
 export default function LandingPage({ site_name }) {
+  document.location = "/home";
   return (
     <>
       <main className="mainComponent">
