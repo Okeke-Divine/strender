10c9a35ebcbf1074ae4eda9bc5fdf006d22cf8bb
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-03 12:24:06 +0100

    Updated the terms and condition

--- src/pages/TermsAndConditions.jsx
+++ src/pages/TermsAndConditions.jsx
@@ -1,7 +1,20 @@
+import TC from "../data/T&C.json";
+
 export default function TermsAndConditions(){
     return (
         <>
-            <div>TermsAndConditions</div>
+            <main className="mainComponent">
+                {TC.sections.map((section, index) => (
+                    <div key={index} className="mb-8">
+                        <h2 className="text-3xl font-bold text-white mb-4">{section.title}</h2>
+                        <ul className="list-disc list-inside">
+                            {section.content.map((item, idx) => (
+                                <li key={idx} className="text-gray-400 mb-2">{item}</li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+            </main>
         </>
-    )
+    );
 }
