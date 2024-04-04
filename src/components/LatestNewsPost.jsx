91274df36c7a3c6f5157fef38b6f0ac8cf02ce99
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-04 12:03:38 +0100

    Updated the latest news component

--- src/components/LatestNewsPost.jsx
+++ src/components/LatestNewsPost.jsx
@@ -1,11 +1,20 @@
 import { Link } from "react-router-dom";
 
-export default function LatestNewsPost({ title, previewText, url, thumbnail }) {
+export default function LatestNewsPost({
+  title,
+  previewText,
+  url,
+  thumbnail,
+  author,
+}) {
   return (
     <div className="hover:opacity-90 duration-300">
       <div className="flex flex-col justify-between h-full">
         <div className="w-full bg-blue-200 h-[200px] rounded-lg p-2"></div>
-        <div className="mt-3 font-bold text-xl text-white">{title}</div>
+        <div>
+          <div className="mt-3 font-bold text-xl text-white">{title}</div>
+          <div className="uppercase text-gray-400">{author}</div>
+        </div>
         <div className="text-gray-400 mt-2">{previewText}</div>
         <div className="mt-2">
           <Link
