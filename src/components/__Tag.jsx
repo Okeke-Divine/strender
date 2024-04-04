684658cc360ad82c82f6f1181c22f3059ae16f05
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-04 13:59:22 +0100

    Updated the post component

---
+++ src/components/__Tag.jsx
@@ -0,0 +1,7 @@
+export default function __Tag({ name }) {
+    return (
+      <>
+        <div className="py-2 px-3 bg-blue-100 text-blue-500 w-fit font-semibold">{name}</div>
+      </>
+    );
+  }
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,4 +1,5 @@
 import DocumentTittle from "../utils/documentTitle";
+import __Tag from "../components/__Tag";
 
 export default function About() {
   return (
@@ -10,7 +11,9 @@ export default function About() {
         <div className="block md:flex bg-white rounded-xl shadow-lg p-5">
           <div className="w-full md:w-4/6">
             {/* category */}
-            <div>Football</div>
+            <div>
+              <__Tag name="Football" />
+            </div>
             {/* post title */}
             <div className="mt-3 font-bold text-2xl">
               NBA Star Giannis Antetokounmpo Launches Foundation to Empower
@@ -18,10 +21,30 @@ export default function About() {
             </div>
             {/* post author, date, views */}
             <div className="flex items-center gap-x-3 font-semibold mt-1">
-                <div>UCL NEWS</div>
-                <div className="text-gray-400">22 Sept 2022</div>
-                <div>.</div>
-                <div className="text-gray-400"> 234 views</div>
+              <div>UCL NEWS</div>
+              <div className="text-gray-500">22 Sept 2022</div>
+              <div>.</div>
+              <div className="text-gray-500"> 234 views</div>
+            </div>
+            {/* post content */}
+            <div className="text-gray-500 mt-3">
+              <p className="mb-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus vel architecto laboriosam dolor rerum nobis cumque optio est. Adipisci magnam id architecto accusantium sunt aliquam aliquid quod natus ullam voluptate.</p>
+              <p className="mb-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus vel architecto laboriosam dolor rerum nobis cumque optio est. Adipisci magnam id architecto accusantium sunt aliquam aliquid quod natus ullam voluptate.</p>
+              <p className="mb-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus vel architecto laboriosam dolor rerum nobis cumque optio est. Adipisci magnam id architecto accusantium sunt aliquam aliquid quod natus ullam voluptate.</p>
+              <p className="mb-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus vel architecto laboriosam dolor rerum nobis cumque optio est. Adipisci magnam id architecto accusantium sunt aliquam aliquid quod natus ullam voluptate.</p>
+              <p className="mb-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus vel architecto laboriosam dolor rerum nobis cumque optio est. Adipisci magnam id architecto accusantium sunt aliquam aliquid quod natus ullam voluptate.</p>
+              <p className="mb-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus vel architecto laboriosam dolor rerum nobis cumque optio est. Adipisci magnam id architecto accusantium sunt aliquam aliquid quod natus ullam voluptate.</p>
+              <p className="mb-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus vel architecto laboriosam dolor rerum nobis cumque optio est. Adipisci magnam id architecto accusantium sunt aliquam aliquid quod natus ullam voluptate.</p>
+              <p className="mb-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus vel architecto laboriosam dolor rerum nobis cumque optio est. Adipisci magnam id architecto accusantium sunt aliquam aliquid quod natus ullam voluptate.</p>
+              <p className="mb-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus vel architecto laboriosam dolor rerum nobis cumque optio est. Adipisci magnam id architecto accusantium sunt aliquam aliquid quod natus ullam voluptate.</p>
+              <p className="mb-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus vel architecto laboriosam dolor rerum nobis cumque optio est. Adipisci magnam id architecto accusantium sunt aliquam aliquid quod natus ullam voluptate.</p>
+              <p className="mb-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus vel architecto laboriosam dolor rerum nobis cumque optio est. Adipisci magnam id architecto accusantium sunt aliquam aliquid quod natus ullam voluptate.</p>
+            </div>
+            {/* post tags */}
+            <div className="flex gap-2 flex-wrap mt-2">
+              <__Tag name="Football" />
+              <__Tag name="Sports" />
+              <__Tag name="Ronaldo" />
             </div>
           </div>
           <div className="mt-5 md:mt-0 w-full md:w-2/6">2</div>
