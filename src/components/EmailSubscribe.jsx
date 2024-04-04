20310715cb15117180c5bab234f36db4e0e90842
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-04 14:07:45 +0100

    Major updates

--- src/components/EmailSubscribe.jsx
+++ src/components/EmailSubscribe.jsx
@@ -2,8 +2,16 @@ export default function EmailSubscribe() {
   return (
     <>
       <div className="font-bold">Subscribe to our resources</div>
-      <div className="text-gray-500 text-sm font-semibold">
+      <div className="text-gray-500 text-sm font-semibold mt-1">
         Add your email to get notified each time we top a new content
+      </div>
+      <div className="mt-5 flex border-2 rounded-lg py-2 px-2 gap-x-2">
+        <div className="w-fit">
+            <i className="fi fi-ts-bars-staggered relative top-[0.1rem]"></i>
+        </div>
+        <div className="w-full">
+            <input className="w-full bg-transparent border-none outline-none" placeholder="Enter your email" />
+        </div>
       </div>
     </>
   );
