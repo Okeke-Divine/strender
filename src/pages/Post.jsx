5255ef20f6efb1a702be3b05b0161735bc5e3a4a
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-04 13:44:30 +0100

    Major updates

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,9 +1,23 @@
 import DocumentTittle from "../utils/documentTitle";
 
-export default function About(){
-    return (
-        <>
-            <div className="text-xl text-white">Http server esponded - Post</div>
-        </>
-    )
+export default function About() {
+  return (
+    <>
+      <main className="mainComponent2 mt-2">
+        <div className="w-full h-[300px] bg-blue-200 rounded-3xl"></div>
+      </main>
+      <main className="mainComponent">
+        <div className="flex bg-white rounded-xl shadow-lg p-5">
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+        test
+      </main>
+    </>
+  );
 }
