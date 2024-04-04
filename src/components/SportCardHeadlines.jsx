f4c25282b866e3603590378b2f4459d3a9247e76
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-04 20:47:32 +0100

    Updated the format date

--- src/components/SportCardHeadlines.jsx
+++ src/components/SportCardHeadlines.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import formatDate from "../utils/formatDate"
 
 export default function SportCardHeadlines({title,newsChannel,date, url}) {
   const style = [
@@ -7,6 +8,7 @@ export default function SportCardHeadlines({title,newsChannel,date, url}) {
     ["bg-blue-500", "text-blue-200"]
   ]
   const col = ["bg-blue-400", "text-white"]
+  const fDate = formatDate(date);
   return (
     <>
     <Link to={url}>
@@ -14,7 +16,7 @@ export default function SportCardHeadlines({title,newsChannel,date, url}) {
         <div className="w-3/4">
           <div className="text-white font-bold text-xl">{title}</div>
           <div className="text-white font-xl mt-3">{newsChannel}</div>
-          <div className={`${col[1]} mt-1`}>{date}</div>
+          <div className={`${col[1]} mt-1`}>{fDate}</div>
         </div>
         <div className="w-1/4"></div>
       </div>
