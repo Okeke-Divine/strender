17b2219fa26b0bc7aa9c399089d725fe359364d8
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-04 13:00:27 +0100

    Updated

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -7,7 +7,7 @@ export default function Discover() {
       <main className="mainComponent">
         <div className="flex rounded-lg items-center">
           <div className="w-full">
-            <input className="w-full px-2 rounded-l-lg bg-white h-10 outline-none border-none" placeholder="Search..." />
+            <input className="w-full pl-5 pr-2 rounded-l-lg bg-white h-10 outline-none border-none" placeholder="Search..." />
           </div>
           <div className="w-fit">
             <button className="w-10 bg-black text-white rounded-r-lg h-10 cursor-pointer">
