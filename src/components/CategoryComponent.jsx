57408bef4f0cc73bc9d0d7c4611411f15643a82d
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-04 13:22:46 +0100

    Updated the category page

--- src/components/CategoryComponent.jsx
+++ src/components/CategoryComponent.jsx
@@ -4,13 +4,13 @@ export default function CategoryComponent({ category }) {
   return (
     <>
       <Link to={"/category/" + category.name}>
-        <div className="flex gap-x-3 w-fit">
+        <div className="flex gap-x-3 w-fit items-center">
           <div className="flex justify-center w-fit">
             <div className="w-16 h-16 rounded-full bg-blue-200"></div>
           </div>
           <div className="w-full">
-            <div className="text-bold">{category.name}</div>
-            <div className="text-gray-400">12M posts</div>
+            <div className="text-bold text-lg">{category.name}</div>
+            <div className="text-gray-400 text-sm">12M posts</div>
           </div>
         </div>
       </Link>
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import api from "../utils/api";
 //components
 import DocumentTittle from "../utils/documentTitle";
+import ComponentTitle from "../components/__global/ComponentTitle";
 import LatestNews from "../components/LatestNews";
 import CategoryComponent from "../components/CategoryComponent";
 
@@ -26,6 +27,9 @@ export default function Category({ lastestNewsDesc }) {
     <>
       <main className="mainComponent">
         <DocumentTittle title="Category" />
+        <div className="mb-5">
+          <ComponentTitle title="Categories" url={null} />
+        </div>
         <div className="text-xl text-white">
           {loading ? (
             <>
