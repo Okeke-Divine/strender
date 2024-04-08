0cb86cd88617caeb4beab3e413176f1038a64b78
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-09 00:38:29 +0100

    Update LatestNewsPost.jsx

--- src/components/LatestNewsPost.jsx
+++ src/components/LatestNewsPost.jsx
@@ -10,7 +10,9 @@ export default function LatestNewsPost({
   return (
     <div className="hover:opacity-90 duration-300">
       <div className="flex flex-col justify-between h-full">
-        <div className="w-full bg-blue-200 h-[200px] rounded-lg p-2"></div>
+        <div className="w-full bg-blue-200 h-[200px] rounded-lg p-2">
+           <img src={thumbnail} \>
+        </div>
         <div>
           <div className="mt-3 font-bold text-xl text-white">{title}</div>
           <div className="uppercase text-gray-400">{author}</div>
